995b827373f4760f3fb6bd560ad9b9285ac4a0cb
skardas <skardas@gmail.com>
2019-11-06 13:14:16 +0300

    first deployment

---
+++ src/main/java/com/pts/mqtt/MqttTester.java
@@ -0,0 +1,18 @@
+package com.pts.mqtt;
+
+import java.util.Random;
+
+public class MqttTester {
+    public static void main(String[] args) throws InterruptedException {
+        SensorEngine sensorEngine = new SensorEngine("localhost", 1883,"test1","test1","sensor/1");
+        new Thread(sensorEngine).start();
+        Random random = new Random();
+
+
+        for (int i = 0; i < 100; i++) {
+            Thread.sleep(500);
+            sensorEngine.send(random.nextDouble() * 60 + random.nextDouble() * 60 + random.nextDouble() * 60
+                    + random.nextDouble() * 60);
+        }
+    }
+}
---
+++ src/main/java/com/pts/mqtt/SensorEngine.java
@@ -0,0 +1,72 @@
+package com.pts.mqtt;
+
+import com.google.gson.Gson;
+import org.eclipse.paho.client.mqttv3.*;
+
+import java.nio.charset.Charset;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+public class SensorEngine implements Runnable {
+    String hostIp;
+    int port;
+    String username;
+    String password;
+    String publisherId = UUID.randomUUID().toString();
+    IMqttClient publisher;
+    String TOPIC = "sensor/1";
+
+    Gson gson;
+
+    public SensorEngine(String hostIp, int port, String username, String password, String TOPIC) {
+        this.hostIp = hostIp;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+        this.TOPIC = TOPIC;
+        gson = new Gson();
+    }
+
+    public void run() {
+        try
+        {
+            publisher = new MqttClient(String.format("tcp://%s:%d",hostIp,port),publisherId);
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setAutomaticReconnect(true);
+            options.setCleanSession(true);
+            options.setConnectionTimeout(10);
+            options.setUserName(username);
+            options.setPassword(password.toCharArray());
+            publisher.connect(options);
+
+            publisher.subscribe(TOPIC, (topic, msg) -> {
+                byte[] payload = msg.getPayload();
+                System.out.println(new String(payload));
+            });
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            return;
+        }
+    }
+    public boolean send(double value)
+    {
+        try {
+            publisher.publish(TOPIC, new MqttMessage(gson.toJson(new SensorData(value)).getBytes(Charset.forName("UTF-8"))));
+        } catch (MqttException e) {
+            return false;
+        }
+        return true;
+    }
+    static class SensorData {
+        int companyId = 1;
+        int sensorId = 1;
+        double value;
+        Date time = Calendar.getInstance().getTime();
+
+        public SensorData(double value) {
+            this.value = value;
+        }
+    }
+}
