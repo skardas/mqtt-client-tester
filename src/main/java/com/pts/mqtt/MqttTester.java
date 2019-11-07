28abb7304fb648f206addc89c19f192bcacd9582
skardas <skardas@gmail.com>
2019-11-07 11:32:29 +0300

    first deployment

--- src/main/java/com/pts/mqtt/MqttTester.java
+++ src/main/java/com/pts/mqtt/MqttTester.java
@@ -1,18 +1,24 @@
 package com.pts.mqtt;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MqttTester {
     public static void main(String[] args) throws InterruptedException {
-        SensorEngine sensorEngine = new SensorEngine("localhost", 1883,"test1","test1","sensor/1");
-        new Thread(sensorEngine).start();
-        Random random = new Random();
+        SensorEngine sensorEngine = new SensorEngine("localhost", 38880,"test1","test1","sensor/1");
 
 
+        ExecutorService service = Executors.newFixedThreadPool(1);
+        service.submit(sensorEngine);
+
+         Random random = new Random();
         for (int i = 0; i < 100; i++) {
             Thread.sleep(500);
             sensorEngine.send(random.nextDouble() * 60 + random.nextDouble() * 60 + random.nextDouble() * 60
                     + random.nextDouble() * 60+ random.nextDouble() * 60+ random.nextDouble() * 60);
         }
+        service.shutdown();
+
     }
 }
